762ce5b4fdee6c40152ff95a741979a11318db6b
Cestnut <hunterkudo@gmail.com>
2025-03-23 19:15:29 +0100

    Fixed the startup making it compatible with the board linker file (needed for NXP toolchain)

--- firmware/startup.s
+++ firmware/startup.s
@@ -1,13 +1,13 @@
-.word stack_top     // Address of the stack_top
-.word _start        // Address of the _start function
+.word __StackTop     // Address of the stack_top
+.word Reset_Handler        // Address of the _start function
 
 // The thumb_func is used to make sure the function is in thumb mode,
 // which is required for the Cortex-M0+.
 .thumb_func 
 
-.global _start
+.global Reset_Handler
 
-_start:
+Reset_Handler:
     mov r0, #0x5
     BL main
     B .
