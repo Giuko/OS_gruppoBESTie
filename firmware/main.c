78f0f0911cc17ad764192323317a47e65f64f198
Cestnut <hunterkudo@gmail.com>
2025-03-23 19:16:21 +0100

    Implemented basic adc usage in firmware (only init and reading(

--- firmware/main.c
+++ firmware/main.c
@@ -1,7 +1,19 @@
 #include "uart.h"
+#include "adc.h"
+#include <stdio.h>
+#include <stdint.h>
+
+
 int main(void) {
-    int i = 0;
+    char message[32];
+    
+    
     UART_init();
+    ADC_init();
+    for(int i=0; i<5; i++){
+        sprintf(message, "prova %d\n", ADC_read());
+        UART_printf(message);
+    }
     UART_printf("Hello World\n");
     return 0;
 }
